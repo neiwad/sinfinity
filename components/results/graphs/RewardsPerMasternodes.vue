23e25bc342467f0410fbbb56764801a21bbb3756
Damien Leeroy <damien@leeroy.ca>
2021-12-20 10:31:22 +0100

    fix import issue

--- components/results/graphs/RewardsPerMasternodes.vue
+++ components/results/graphs/RewardsPerMasternodes.vue
@@ -1,47 +1,47 @@
 <template>
-    <div class="flex flex-col justify-between w-full h-full">
-        <h2 class="font-bold text-4xl">Rewards by masternodes</h2>
-        <div class="w-full h-[400px] relative">
-            <canvas id="rpm_chart" ref="root" responsive></canvas>
-        </div>
+  <div class="flex flex-col justify-between w-full h-full">
+    <h2 class="font-bold text-4xl">Rewards by masternodes</h2>
+    <div class="w-full h-[400px] relative">
+      <canvas id="rpm_chart" ref="root" responsive></canvas>
     </div>
+  </div>
 </template>
 
 <script setup>
-import Chart from 'chart.js/auto'
-import { onMounted } from "vue"
-const { masternodes, masternodesLoaded } = useMasternodes()
-const root = ref(null)
+import Chart from "chart.js/auto/auto.js";
+import { onMounted } from "vue";
+const { masternodes, masternodesLoaded } = useMasternodes();
+const root = ref(null);
 onMounted(() => {
-    const ctx = root.value.getContext('2d')
-    const data = {
-        labels: [],
-        datasets: [
-            {
-                label: 'Rewards by Masternodes',
-                type: 'bar',
-                data: [],
-                backgroundColor: []
-            },
-            {
-                label: 'Burned',
-                type: 'line',
-                data: [],
-                fill: false,
-                borderColor: 'rgb(54, 162, 235)'
-            }]
-    }
-    masternodes.value.forEach((_masternode, index) => {
-        data.labels.push(`Node ${index}`)
-        data.datasets[0].data.push(_masternode.rewards)
-        data.datasets[0].backgroundColor.push(_masternode.color)
-        data.datasets[1].data.push(_masternode.burned)
-    })
+  const ctx = root.value.getContext("2d");
+  const data = {
+    labels: [],
+    datasets: [
+      {
+        label: "Rewards by Masternodes",
+        type: "bar",
+        data: [],
+        backgroundColor: [],
+      },
+      {
+        label: "Burned",
+        type: "line",
+        data: [],
+        fill: false,
+        borderColor: "rgb(54, 162, 235)",
+      },
+    ],
+  };
+  masternodes.value.forEach((_masternode, index) => {
+    data.labels.push(`Node ${index}`);
+    data.datasets[0].data.push(_masternode.rewards);
+    data.datasets[0].backgroundColor.push(_masternode.color);
+    data.datasets[1].data.push(_masternode.burned);
+  });
 
-    new Chart(ctx, {
-        type: 'bar',
-        data: data
-
-    });
-})
+  new Chart(ctx, {
+    type: "bar",
+    data: data,
+  });
+});
 </script>
--- components/results/graphs/RewardsPerMonth.vue
+++ components/results/graphs/RewardsPerMonth.vue
@@ -1,62 +1,66 @@
 <template>
-    <div class="flex flex-col justify-between w-full h-full">
-        <h2 class="font-bold text-4xl">Rewards by months</h2>
-        <div class="w-full h-[400px] relative">
-            <canvas id="rewards_per_month_chart" ref="root" responsive></canvas>
-        </div>
+  <div class="flex flex-col justify-between w-full h-full">
+    <h2 class="font-bold text-4xl">Rewards by months</h2>
+    <div class="w-full h-[400px] relative">
+      <canvas id="rewards_per_month_chart" ref="root" responsive></canvas>
     </div>
+  </div>
 </template>
 
 <script setup>
-import Chart from 'chart.js/auto'
-import { onMounted } from "vue"
-const { masternodes } = useMasternodes()
+import Chart from "chart.js/auto/auto.js";
+import { onMounted } from "vue";
+const { masternodes } = useMasternodes();
 const reducer = (previousValue, currentValue) => previousValue + currentValue;
-const root = ref(null)
+const root = ref(null);
 onMounted(() => {
-    const ctx = root.value.getContext('2d')
+  const ctx = root.value.getContext("2d");
 
-    const data = {
-        labels: [],
-        datasets: []
-    }
+  const data = {
+    labels: [],
+    datasets: [],
+  };
 
-    const history = []
-    masternodes.value.forEach(m => {
-        history.push(...m.history)
-    })
-    const months = [...new Set(history.map(h => {
-        const date = new Date(h.date)
-        const niceMonth = `${date.getFullYear()}-${date.getMonth() + 1}`
-        return niceMonth
-    }))]
+  const history = [];
+  masternodes.value.forEach((m) => {
+    history.push(...m.history);
+  });
+  const months = [
+    ...new Set(
+      history.map((h) => {
+        const date = new Date(h.date);
+        const niceMonth = `${date.getFullYear()}-${date.getMonth() + 1}`;
+        return niceMonth;
+      })
+    ),
+  ];
 
-    data.labels = months
+  data.labels = months;
 
-    masternodes.value.forEach((_masternode, index) => {
-        const object = {
-            label: `Node ${index}`,
-            type: 'bar',
-            data: [],
-            backgroundColor: _masternode.color
-        }
-        months.forEach(_month => {
-            const test = _masternode.history.filter(_h => {
-                const date = new Date(_h.date)
-                const niceDate = `${date.getFullYear()
-                    }-${date.getMonth() + 1}`
-                return niceDate === _month
-            })
-            const rewards = test.length ? test.map(t => t.amount).reduce(reducer) : 0
-            object.data.push(rewards)
-        })
-        data.datasets.push(object)
-    })
-
-    new Chart(ctx, {
-        type: 'bar',
-        data: data
-
+  masternodes.value.forEach((_masternode, index) => {
+    const object = {
+      label: `Node ${index}`,
+      type: "bar",
+      data: [],
+      backgroundColor: _masternode.color,
+    };
+    months.forEach((_month) => {
+      const test = _masternode.history.filter((_h) => {
+        const date = new Date(_h.date);
+        const niceDate = `${date.getFullYear()}-${date.getMonth() + 1}`;
+        return niceDate === _month;
+      });
+      const rewards = test.length
+        ? test.map((t) => t.amount).reduce(reducer)
+        : 0;
+      object.data.push(rewards);
     });
-})
+    data.datasets.push(object);
+  });
+
+  new Chart(ctx, {
+    type: "bar",
+    data: data,
+  });
+});
 </script>
